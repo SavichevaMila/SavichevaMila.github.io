835ee4e5ef1440a9e6f9cef8372bb136532571fc
savichevamila <sem13007@buyi.edu>
2019-05-31 00:36:20 +0300

    update

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,9 +1,8 @@
 function currentDate() {
-    var today = new Date();
-    var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var today = new Date();    
     var monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];    
-    var nowDate = weekday[today.getDay()];
+    var nowDate = today.getDay();
     var day = today.getDate();     // Get current date
     var month = monthNames[today.getMonth()]; // current month
     var year = today.getFullYear();
