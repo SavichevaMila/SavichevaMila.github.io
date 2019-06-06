044928ddb065c910090ffe0ff85f8f0143b18ab7
savichevamila <sem13007@buyi.edu>
2019-06-06 14:27:17 +0300

    update

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -1,10 +1,5 @@
-var temp = parseFloat(document.getElementById("t").innerHTML);
-    var speed = parseFloat(document.getElementById("s").innerHTML);
-    var chill = windChill(temp, speed);
-console.log("hello");
-    document.getElementById("windChill").innerHTML = chill.toFixed(1); 
-    function windChill(t, s) {
-    var sp = Math.pow(s, 0.16);
-    var result = 35.74 + (0.6215 * t) - (35.75 * sp) + (0.4275 * t * sp);
-    return result;
-}
+var t = parseFloat(document.getElementById("temp").innerHTML);
+var s = parseFloat(document.getElementById("speed").innerHTML);
+var result = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
+document.getElementById('output').innerHTML=result.toFixed(1)+"&#8457";
+
