f4a7d7bcda4fb0890c10b6b4cfe3449bafd38e04
savichevamila <sem13007@buyi.edu>
2019-07-09 22:24:47 +0300

    update

--- assignments/weathersite/js/events.js
+++ assignments/weathersite/js/events.js
@@ -1,30 +1,31 @@
-var section = document.querySelector('aside');
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var div = document.getElementById("events");
+
+var requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 var request = new XMLHttpRequest();
-
-request.open('GET', requestURL);
-request.responseType = 'json';
+request.open("GET", requestURL);
+request.responseType = "json";
 request.send();
-
-request.onload = function () {
-    var townData = request.response;
-    showData(townData);
-}
-
+request.onload = function() {
+  var data = request.response;
+  showData(data);
+};
 function showData(jsonObj) {
-    var towns = jsonObj['towns'];
+  var eventData = jsonObj["towns"];
 
-    for (var i = 0; i < towns.length; i++) {
-        var myList = document.createElement('ul');
-        if (towns[i].name == "Preston") {
-            var events = towns[i].events;
-            for (var j = 0; j < events.length; j++) {
-                var listItem = document.createElement('li');
-                listItem.textContent = events[j];
-                myList.appendChild(listItem);
+  for (var i = 0; i < eventData.length; i++) {
+    if (eventData[i].name == "Preston") {
+      //   console.log(string);
 
-            }
-            section.appendChild(myList);
-        }
+      var myul = document.createElement("ul");
+
+      var events = eventData[i].events;
+      for (var k = 0; k < events.length; k++) {
+        var myli = document.createElement("li");
+        myli.textContent = events[k];
+        myul.appendChild(myli);
+      }
+
+      div.appendChild(myul);
     }
+  }
 }
