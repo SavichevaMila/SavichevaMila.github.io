6799da061cc32937116538d4cf8a687b86cb32fd
savichevamila <sem13007@buyi.edu>
2019-07-23 00:40:35 +0300

    update

--- assignments/termproject/js/weatherapibrigham.js
+++ assignments/termproject/js/weatherapibrigham.js
@@ -1,6 +1,6 @@
 let weatherRequest = new XMLHttpRequest();
 let apiURLstring =
-  "https://api.openweathermap.org/data/2.5/weather?zip=84302&units=imperial&APPID=62e87ca2c7400a7b300db9de2e2390c9";
+  "https://api.openweathermap.org/data/2.5/weather?id=5771960&units=imperial&APPID=62e87ca2c7400a7b300db9de2e2390c9";
   weatherRequest.open('Get', apiURLstring, true);
   weatherRequest.send();
   
