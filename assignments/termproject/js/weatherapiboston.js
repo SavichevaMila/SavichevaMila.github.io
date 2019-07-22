cdf55b0bcea81f8ba26de5480ca56d31e7f95f2e
savichevamila <sem13007@buyi.edu>
2019-07-23 00:14:02 +0300

    update

--- assignments/termproject/js/weatherapiboston.js
+++ assignments/termproject/js/weatherapiboston.js
@@ -1,34 +1,18 @@
 let weatherRequest = new XMLHttpRequest();
 let apiURLstring =
   "https://api.openweathermap.org/data/2.5/weather?zip=02478&units=imperial&APPID=62e87ca2c7400a7b300db9de2e2390c9";
-weatherRequest.open("GET", apiURLstring, true);
-weatherRequest.send();
-
-weatherRequest.onload = function() {
-  var weatherData = JSON.parse(weatherRequest.responseText);
-  //   console.log(weatherData);
-
-  document.getElementById("currently2").innerHTML = weatherData.weather[0].main;
-  document.getElementById("temp2").innerHTML = weatherData.main.temp;
-  document.getElementById("windSpeed2").innerHTML = weatherData.wind.speed;
-
-  var tempF = parseInt(document.getElementById("temp2").innerHTML);
-  var speed = parseInt(document.getElementById("windSpeed2").innerHTML);
-  var chill = windChill(weatherData.main.temp, weatherData.wind.speed);
-
-  var digits = 2;
-  var multiplier = Math.pow(10, digits);
-  chill = Math.round(chill * multiplier) / multiplier;
-  document.getElementById("windchill2").innerHTML = chill;
-
-  function windChill(tempF, speed) {
-    result =
-      35.74 +
-      0.6215 * tempF -
-      35.75 * Math.pow(speed, 0.16) +
-      0.4275 * tempF * Math.pow(speed, 0.16);
-    return result;
+  weatherRequest.open('Get', apiURLstring, true);
+  weatherRequest.send();
+  
+  weatherRequest.onload = function () {
+      let weatherData = JSON.parse(weatherRequest.responseText);
+      console.log(weatherData);
+     
+  
+      document.getElementById('current4').innerHTML = weatherData.weather[0].main;
+      document.getElementById('high4').innerHTML = weatherData.main.temp_max;
+      document.getElementById('humidity4').innerHTML = weatherData.main.humidity;
+      document.getElementById('speed4').innerHTML = weatherData.wind.speed;
+      displayWindChill()
+      
   }
-  document.getElementById("windchill2").innerHTML = chill;
-  document.getElementById("humidity2").innerHTML = weatherData.main.humidity;
-};
