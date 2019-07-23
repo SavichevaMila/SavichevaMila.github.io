446a8a3e26cfdd51a88fe9104ac75bbb287fdc90
savichevamila <sem13007@buyi.edu>
2019-07-23 14:34:40 +0300

    update

--- assignments/termproject/js/weatherapiboise.js
+++ assignments/termproject/js/weatherapiboise.js
@@ -1,35 +1,35 @@
-let weatherRequest = new XMLHttpRequest();
-let apiURLstring =
+let weatherRequest3 = new XMLHttpRequest();
+let apiURLstring3 =
   "https://api.openweathermap.org/data/2.5/weather?id=5586437&units=imperial&APPID=62e87ca2c7400a7b300db9de2e2390c9";
-  weatherRequest.open('Get', apiURLstring, true);
-  weatherRequest.send();
+  weatherRequest3.open('Get', apiURLstring3, true);
+  weatherRequest3.send();
   
-  weatherRequest.onload = function() {
-    var weatherData = JSON.parse(weatherRequest.responseText);
+  weatherRequest3.onload = function() {
+    var weatherData3 = JSON.parse(weatherRequest3.responseText);
     //   console.log(weatherData);
   
-    document.getElementById("currently3").innerHTML = weatherData.weather[0].main;
-    document.getElementById("temp3").innerHTML = weatherData.main.temp;
-    document.getElementById("windSpeed3").innerHTML = weatherData.wind.speed;
+    document.getElementById("currently3").innerHTML = weatherData3.weather[0].main;
+    document.getElementById("temp3").innerHTML = weatherData3.main.temp;
+    document.getElementById("windSpeed3").innerHTML = weatherData3.wind.speed;
   
-    var tempF = parseInt(document.getElementById("temp3").innerHTML);
-    var speed = parseInt(document.getElementById("windSpeed3").innerHTML);
-    var chill = windChill(weatherData.main.temp, weatherData.wind.speed);
+    var tempF3 = parseInt(document.getElementById("temp3").innerHTML);
+    var speed3 = parseInt(document.getElementById("windSpeed3").innerHTML);
+    var chill3 = windChill(weatherData3.main.temp, weatherData3.wind.speed);
   
-    var digits = 2;
-    var multiplier = Math.pow(10, digits);
-    chill = Math.round(chill * multiplier) / multiplier;
-    document.getElementById("windchill3").innerHTML = chill;
+    var digits3 = 2;
+    var multiplier3 = Math.pow(10, digits3);
+    chill = Math.round(chill3 * multiplier3) / multiplier3;
+    document.getElementById("windchill3").innerHTML = chill3;
   
-    function windChill(tempF, speed) {
-      result =
+    function windChill(tempF3, speed3) {
+      result3 =
         35.74 +
-        0.6215 * tempF -
-        35.75 * Math.pow(speed, 0.16) +
-        0.4275 * tempF * Math.pow(speed, 0.16);
-      return result;
+        0.6215 * tempF3 -
+        35.75 * Math.pow(speed3, 0.16) +
+        0.4275 * tempF * Math.pow(speed3, 0.16);
+      return result3;
     }
-    document.getElementById("windchill3").innerHTML = chill;
-    document.getElementById("humidity3").innerHTML = weatherData.main.humidity;
+    document.getElementById("windchill3").innerHTML = chill3;
+    document.getElementById("humidity3").innerHTML = weatherData3.main.humidity;
   };
   
